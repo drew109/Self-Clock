3a1454c318a0957e456737764ec5bcf746684937
ykyk21222 <andrewrules903@gmail.com>
2022-03-10 21:57:31 -0500

    added the clock function and called it

--- clcok.py
+++ clcok.py
@@ -8,8 +8,16 @@ root = Tk()
 root.title("Clock")
 
 
+def time():
+    string = strftime('%H:%M:%S %p')
+    label.config(text=string)
+    label.after(1000, time)
+
+
 label = Label(root, font=("Arial", 80), background="black", foreground='green')
 label.pack(anchor='center')
+
+time()
 
 
 mainloop()
